0f8bbc25e83cd78350c8e35f39b28e1eab874653
chjjh0 <chjjh0@gmail.com>
2019-01-02 16:01:37 +0900

    0102

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -21,6 +21,8 @@ class Login extends Component {
             <h1>
               <img src={Logo} alt="logoImage"></img>
             </h1>
+            <input type="text" placeholder="e-mail" className="inputTest"/>
+            <input type="password" placeholder="Password" className="inputTest"/>
             <div className="signin">로그인</div>
             <hr/>
             <div className="signup">이메일로 가입</div>
