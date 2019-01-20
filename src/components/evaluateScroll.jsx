f6fe55f50739698c1d5734ca93f8c539e31ead1c
chjjh0 <chjjh0@gmail.com>
2019-01-21 04:46:51 +0900

    0121

--- src/components/evaluateScroll.jsx
+++ src/components/evaluateScroll.jsx
@@ -38,8 +38,13 @@ class infiniteScl extends Component {
                     '<div class="evaluateHover">' +
                         '<h2 class="evaluateVideoTitle">' + this.props.videoDesc[this.videoIndex].title + '</h2>' +
                         '<p class="evaluateReleaseYear">' + this.props.videoDesc[this.videoIndex].releaseYear  + '</p>' +
-                        '<div class="starEvalBefore">★★★★★</div>' +
-                        '<div class="starEvalAfter">★★★★★</div>' +
+                        '<p class="star">' + 
+                            '<a href="#">★</a>' +
+                            '<a href="#">★</a>' +
+                            '<a href="#">★</a>' +
+                            '<a href="#">★</a>' +
+                            '<a href="#">★</a>' +
+                        '</p>' +
                     '</div>' +
                 '</div>' +
             '</div>'
@@ -91,19 +96,57 @@ class infiniteScl extends Component {
             $(function() {
                 $(".evaluateVideoImgArea").hover(function() {
                     // mouseEnter
-                    $(".starEvalAfter").css({"opacity": "0"});
-                    $(document).on('mouseenter', '.starEvalAfter', function() {
-                        console.log('enter')
+                    //$(".starEvalAfter").css({"opacity": "0"});
+                    $(document).on('mouseenter', 'p.star', function() {
+                        //console.log('enter')
                         var offset = $(this).offset();
-                        $(".starEvalAfter").css({"opacity": "0"});
                         $(this).on('mousemove', function(event){
                             // star init
                             var mouseNow = (event.pageX - offset.left);
-                            console.log('X Axis : ' + event.pageX);
-                            console.log('mouseNow : ', mouseNow);
-                            if(mouseNow >= 30 || mouseNow <=45) {
-                                                                
-                            }
+                            //console.log('X Axis : ' + event.pageX);
+                            //console.log('mouseNow : ', mouseNow);
+                            // 별점 초기화
+                            $(this).find("a").removeClass("half full");
+                            if(mouseNow >= 20 && mouseNow <= 40) {
+                                // this = p.star
+                                // 1 star half
+                                $(this).find("a:eq(0)").addClass("half");
+                                $(this).find("a:eq(0)").on('click', function(){
+                                    console.log('index: ', $(this).index($(this).parent()))
+                                })
+                            } else if(mouseNow >= 41 && mouseNow <= 60) {
+                                // 1 star full
+                                $(this).find("a:eq(0)").addClass("full");
+                            } else if(mouseNow >= 61 && mouseNow <= 85) {
+                                // 2 star half
+                                $(this).find("a:eq(1)").addClass("half");
+                                $(this).find("a:eq(1)").prevAll("a").addClass("full");
+                            } else if(mouseNow >= 86 && mouseNow <= 100) {
+                                // 2 star full
+                                $(this).find("a:eq(2)").prevAll("a").addClass("full");
+                            } else if(mouseNow >= 101 && mouseNow <= 130) {
+                                // 3 star half
+                                $(this).find("a:eq(2)").addClass("half");
+                                $(this).find("a:eq(2)").prevAll("a").addClass("full");
+                            } else if(mouseNow >= 131 && mouseNow <= 150) {
+                                // 3 star full
+                                $(this).find("a:eq(3)").prevAll("a").addClass("full");
+                            } else if(mouseNow >= 151 && mouseNow <= 170) {
+                                // 4 star half
+                                $(this).find("a:eq(3)").addClass("half");
+                                $(this).find("a:eq(3)").prevAll("a").addClass("full");
+                            } else if(mouseNow >= 171 && mouseNow <= 200) {
+                                // 4 star full
+                                $(this).find("a:eq(4)").prevAll("a").addClass("full");
+                            } else if(mouseNow >= 201 && mouseNow <= 220) {
+                                // 5 star half
+                                $(this).find("a:eq(4)").addClass("half");
+                                $(this).find("a:eq(4)").prevAll("a").addClass("full");
+                            } else if(mouseNow >= 221 && mouseNow <= 250) {
+                                // 5 star full
+                                $(this).find("a:eq(4)").addClass("full");
+                                $(this).find("a:eq(4)").prevAll("a").addClass("full");
+                            } 
                         });
                     });
                     // mouseLeave
